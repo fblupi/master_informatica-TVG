d5d224555bf5f4ea3aef784823ef85f716057d54
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-03-04 00:12:13 +0100

    Hello world closes #1

---
+++ script1/task1/src/HelloWorld.cxx
@@ -0,0 +1,13 @@
+#include "itkImage.h"
+#include <iostream>
+
+int main()
+{
+  typedef itk::Image< unsigned short, 3 > ImageType;
+
+  ImageType::Pointer image = ImageType::New();
+
+  std::cout << "ITK Hello World !" << std::endl;
+
+  return EXIT_SUCCESS;
+}
