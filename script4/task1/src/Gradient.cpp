e26f65880813a1a41d2ca0856d033ad3e004776b
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-03-31 18:48:30 +0200

    Task 1 closes 20#

---
+++ script4/task1/src/Gradient.cpp
@@ -0,0 +1,52 @@
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+
+#include "itkGradientMagnitudeImageFilter.h"
+#include "itkGradientMagnitudeRecursiveGaussianImageFilter.h"
+
+#include "QuickView.h"
+
+using namespace std;
+
+int main (int argc, char * argv[])
+{
+	if (argc < 2) {
+		cerr << "Usage: " << endl;
+		cerr << argv[0] << "   inputImageFile " << endl;
+		return EXIT_FAILURE;
+	}
+
+	typedef float InputPixelType;
+	typedef float OutputPixelType;
+
+	typedef itk::Image<InputPixelType, 2> InputImageType;
+	typedef itk::Image<OutputPixelType, 2> OutputImageType;
+
+	typedef itk::ImageFileReader<InputImageType> ReaderType;
+	ReaderType::Pointer reader = ReaderType::New();
+	reader->SetFileName(argv[1]);
+
+	typedef itk::GradientMagnitudeImageFilter<InputImageType, OutputImageType> GradientMagnitudeFilterType;
+	GradientMagnitudeFilterType::Pointer gradientMagnitudeFilter = GradientMagnitudeFilterType::New();
+	gradientMagnitudeFilter->SetInput(reader->GetOutput());
+
+	gradientMagnitudeFilter->Update();
+
+	typedef itk::GradientMagnitudeRecursiveGaussianImageFilter<InputImageType, OutputImageType> GradientMagnitudeRecursiveGaussianFilterType;
+	GradientMagnitudeRecursiveGaussianFilterType::Pointer gradientMagnitudeRecursiveGaussianFilter = GradientMagnitudeRecursiveGaussianFilterType::New();
+	gradientMagnitudeRecursiveGaussianFilter->SetInput(reader->GetOutput());
+
+	const double sigma = 0.5;
+	gradientMagnitudeRecursiveGaussianFilter->SetSigma(sigma);
+
+	gradientMagnitudeRecursiveGaussianFilter->Update();
+
+	QuickView viewer;
+	viewer.SetNumberOfColumns(3);
+	viewer.AddImage(reader->GetOutput());
+	viewer.AddImage(gradientMagnitudeFilter->GetOutput());
+	viewer.AddImage(gradientMagnitudeRecursiveGaussianFilter->GetOutput());
+	viewer.Visualize();
+
+	return EXIT_SUCCESS;
+}
